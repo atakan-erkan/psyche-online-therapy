e83e0409c96020d809f77572f10e854ad15ed065
Atakan Erkan <atakan.erkan@ogr.dpu.edu.tr>
2023-03-15 02:17:34 +0300

    Footer Change

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,99 +4,52 @@ function Footer() {
   return (
     <footer id="footer">
       <hr />
-      <div class="container py-5">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="footer-logo">
-              <a href="https://www.hiwellapp.com">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="footer-logo">
+              <a href="/">
                 <img
-                  width="131"
-                  height="58"
+                  width="150"
+                  height="150"
                   loading="lazy"
-                  src="https://www.hiwellapp.com/assets/images/hiwell-logo.svg"
+                  src="img/PSYCHE-2.png"
                   alt="hiwell logo"
                 />
               </a>
             </div>
-            <div class="footer-company-description mt-4">
+            <div className="footer-company-description mt-4">
               500'den fazla uzman psikolog kadromuzla ihtiyacınız olan her
               konuda anında online terapi ve psikolojik danışmanlık!
             </div>
-
-            <div class="footer-nav-title mt-4">Uygulamayı indirin</div>
-            <div class="row mb-4 mx-0">
-              <div class="col-4 p-1 col-sm-4 mt-2">
-                <a href="https://apps.apple.com/us/app/hiwell-psikolog-online/id1506664037">
-                  <img
-                    width="131"
-                    height="39"
-                    loading="lazy"
-                    src="https://www.hiwellapp.com/assets/images/app-apple.svg"
-                    alt="app store download banner"
-                    class="img-fluid"
-                  />
-                </a>
-              </div>
-              <div class="col-4 p-1 col-sm-4 mt-2">
-                <a href="https://play.google.com/store/apps/details?id=com.hiwell">
-                  <img
-                    width="131"
-                    height="39"
-                    loading="lazy"
-                    src="https://www.hiwellapp.com/assets/images/app-android.svg"
-                    alt="playstore download banner"
-                    class="img-fluid"
-                  />
-                </a>
-              </div>
-            </div>
           </div>
 
-          <div class="col-md-2 mb-4 mb-md-0 align-items-md-center footer-col-hiwell">
+          <div className="col-md-2 mb-4 mb-md-0 align-items-md-center footer-col-hiwell">
             <div>
-              <div class="footer-nav-title">Hiwell</div>
-              <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    href="https://www.hiwellapp.com/psikologlar"
-                  >
+              <div className="footer-nav-title">Psyche</div>
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
                     Psikologlar
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    href="https://www.hiwellapp.com/#nasil-calisir"
-                  >
+                <li className="nav-item">
+                  <a className="nav-link" href="/#nasil-calisir">
                     Nasıl Çalışır
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://www.hiwellapp.com/kurumsal">
-                    Kurumsal
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://www.hiwellapp.com/blog">
-                    Blog
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://www.hiwellapp.com/sss">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
                     S.S.S.
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    href="https://www.hiwellapp.com/hakkimizda"
-                  >
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
                     Hakkımızda
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://www.hiwellapp.com/iletisim">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
                     Bize Ulaşın
                   </a>
                 </li>
@@ -104,253 +57,81 @@ function Footer() {
             </div>
           </div>
 
-          <div class="col-md-2 mb-4 mb-md-0">
-            <div class="footer-nav-title">Popüler Başlıklar</div>
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/blog/dopamin-nedir-dopamin-ne-ise-yarar"
-                >
-                  Dopamin Nedir? Ne İşe Yarar?
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/blog/sosyal-medyanin-olumlu-olumsuz-etkileri"
-                >
-                  Sosyal Medyanın Etkileri
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/blog/ozguven-dusuklugu-sebepleri-ve-ozguven-kazanma-yollari"
-                >
-                  Özgüven Düşüklüğü Sebepleri
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/blog/oz-sefkat"
-                >
-                  Öz Şefkat Nedir?
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/blog/irade-nasil-olusur-ve-nasil-guclendirilir"
-                >
-                  İrade Nasıl Güçlendirilir?
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/blog/serotonin-nedir-mutluluk-hormonu-ne-ise-yarar-nasil-artirilir"
-                >
-                  Serotonin Nedir? Nasıl Artırılır?
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/blog/baglanma-stilleri-nelerdir-iliskilerimizi-nasil-etkiler"
-                >
-                  Bağlanma Stilleri Nelerdir?
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/blog/sema-kimyasi-neden-benzer-kisilere-cekim-hissederiz"
-                >
-                  Şema Kimyası Nedir?
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="col-md-2 mb-4 mb-md-0">
-            <div class="footer-nav-title">Psikolojik Testler</div>
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/testler/depresyon-testi"
-                >
+          <div className="col-md-2 mb-4 mb-md-0">
+            <div className="footer-nav-title">Psikolojik Testler</div>
+            <ul className="nav flex-column">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Depresyon Testi
                 </a>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/testler/anksiyete-(kaygi)-testi"
-                >
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Anksiyete (Kaygı) Testi
                 </a>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/testler/stres-testi"
-                >
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Stres Testi
                 </a>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/testler/travmatik-yas-testi"
-                >
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Travmatik Yas Testi
                 </a>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/testler/travma-sonrasi-stres-bozuklugu-testi"
-                >
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Travma Sonrası Stres Bozukluğu Testi
                 </a>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/testler/alkol-bagimliligi-testi"
-                >
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Alkol Bağımlılığı Testi
                 </a>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/testler/sosyal-kaygi-testi"
-                >
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Sosyal Kaygı Testi
                 </a>
               </li>
             </ul>
           </div>
 
-          <div class="col-md-2 mb-4 mb-md-0">
-            <div class="footer-nav-title">Şartlar ve Gizlilik</div>
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/gizlilik-sozlesmesi"
-                >
+          <div className="col-md-2 mb-4 mb-md-0">
+            <div className="footer-nav-title">Şartlar ve Gizlilik</div>
+            <ul className="nav flex-column">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Gizlilik Politikası
                 </a>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/kullanim-sozlesmesi"
-                >
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Kullanıcı Sözleşmesi
                 </a>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://www.hiwellapp.com/cerez-politikasi"
-                >
+              <li className="nav-item">
+                <a className="nav-link" href="/">
                   Çerez Politikası
                 </a>
               </li>
 
-              <li class="nav-item">
-                <a class="nav-link" href="https://www.hiwellapp.com/kvkk">
+              <li className="nav-item">
+                <a className="nav-link" href="h">
                   Aydınlatma Metni
                 </a>
               </li>
             </ul>
-            <div class="mt-2">
+            <div className="mt-2">
               <img
                 loading="lazy"
-                src="https://www.hiwellapp.com/assets/images/etbis.png"
+                src="img/qrcode_www.psychetherapy.online.png"
                 width="70"
                 height="81"
                 alt="Etbis QR"
-                class="img-fluid"
+                className="img-fluid"
               />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="container footer-notice">
-        <div class="row">
-          <div class="col-12">
-            <div class="text-center pb-4 ">
-              Eğer kriz anında olduğunuzu ya da başka bir kişinin tehlikede
-              olduğunu düşünüyorsanız, bu siteyi kullanmayınız. Hemen doğru
-              yardımı almak için{" "}
-              <span
-                role="button"
-                class="text-decoration-underline cur"
-                id="emergency-contact-button"
-              >
-                bu kaynaklara
-              </span>{" "}
-              başvurunuz.
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="container copyright-seciton">
-        <div class="row">
-          <div class="col-12 d-flex justify-content-between align-items-center">
-            <div>© Hiwell</div>
-
-            <div>
-              <ul class="nav social-links">
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    href="https://www.facebook.com/hiwellapp/"
-                  >
-                    <img
-                      width="30"
-                      height="14"
-                      loading="lazy"
-                      src="https://www.hiwellapp.com/assets/images/social-facebook-dark.svg"
-                      alt="facebook icon"
-                    />
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://twitter.com/hiwellapp">
-                    <img
-                      width="30"
-                      height="14"
-                      loading="lazy"
-                      src="https://www.hiwellapp.com/assets/images/social-twitter-dark.svg"
-                      alt="twitter icon"
-                    />
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    href="https://www.instagram.com/hiwellpsikoloji/"
-                  >
-                    <img
-                      width="30"
-                      height="14"
-                      loading="lazy"
-                      src="https://www.hiwellapp.com/assets/images/social-instagram-dark.svg"
-                      alt="instagram icon"
-                    />
-                  </a>
-                </li>
-              </ul>
             </div>
           </div>
         </div>
---
+++ src/Components/Footer/FooterInfo.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Footer.css";
+
+function FooterInfo() {
+  return (
+    <div className="footer-info">
+      <div className="container py-4 px-2">
+        <div class="row">
+          <div class="col-md-8 justify-content-center align-self-center">
+            <p class="footer-info-title">Uyarı</p>
+            <p class="footer-info-text">
+              Eğer kriz anında olduğunuzu ya da başka bir kişinin tehlikede
+              olduğunu düşünüyorsanız, bu siteyi kullanmayınız. Hemen doğru
+              yardımı almak için butona tıklayınız.
+            </p>
+          </div>
+          <div class="col-md-4 text-center justify-content-center align-self-center">
+            <button
+              type="button"
+              class="footer-info-button"
+              aria-label="Yardım"
+              id="#helpButton"
+              data-toggle="modal"
+              data-target="#helpModal"
+            >
+              Yardım
+            </button>
+          </div>
+          <div class="col-12 ">
+            <div className="footer-info-text">© Psyche</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FooterInfo;
