ebed39d889072e3c05480eb1d552e6ac2b281b92
mehmetcandinc <dincmehmetcann@gmail.com>
2023-03-17 17:00:35 +0300

    added datial doctor comp

--- src/Components/routes/psychologists/SingleP.jsx
+++ src/Components/routes/psychologists/SingleP.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const SingleP = ({ user }) => {
+  const handleClick = () => {
+    <div>{user}</div>;
+  };
   return (
     <div className=" container mt-5">
       <div className="row">
         <div className="col-sm-12 text-center">
           <img className="rounded" src={user.picture.large} alt="" />
           <h3>
-            {user.name.first} {user.name.last}
+            Dr. {user.name.first} {user.name.last}@{user.login.username}
           </h3>
+          <h5>
+            {user.location.city} | {user.location.country}
+          </h5>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+          <p>Akademik Yıl ({user.registered.age})</p>
+          <button onClick={() => handleClick()}>DAHA FAZLA TANI</button>
         </div>
       </div>
     </div>
