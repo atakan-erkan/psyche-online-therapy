b9aa3a73bc5dfc3880209fcf09110938818ef817
Atakan Erkan <atakan.erkan@ogr.dpu.edu.tr>
2023-04-16 16:51:12 +0300

    Psychologists - change

--- client/src/scenes/psychologistsPage/SingleP.jsx
+++ client/src/scenes/psychologistsPage/SingleP.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./Psychologists.css";
-import { useSelector } from "react-redux";
 
-const SingleP = () => {
-  const user = useSelector((state) => state.user);
-
+const SingleP = ({ user }) => {
   return (
     <div>
       <div className="card single">
         <img
-          src={user.picturePath}
+          src={user.picture.large}
           className="card-img-top rounded-circle shadow-4-strong"
           alt="..."
         />
@@ -20,21 +17,24 @@ const SingleP = () => {
             style={{ textDecoration: "none", color: "black" }}
           >
             <h5 className="card-title fw-bolder">
-              {user.firstName} {user.lastName}
+              {user.name.first} {user.name.last}
             </h5>
           </a>
-          <p className="text-primary">{user.occupation}</p>
+          <p className="text-primary">Uzman Klinik Psikolog</p>
           <h6 className="card-text">
-            {user.location} | {user.country}
+            {user.location.city} | {user.location.country}
           </h6>
           <h6 className="fw-bolder">Hakkında</h6>
           <p className="card-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Consectetur, beatae.
           </p>
+          <p className="card-text mb-4">Deneyim: {user.registered.age} yıl</p>
           <h6 className="fw-bolder">İletişim</h6>
           <i className="fa-sharp fa-solid fa-at"></i>
           <p className="card-text email">{user.email}</p> {/*bak*/}
+          <i className="fa-solid fa-phone"></i>
+          <p className="card-text mb-4">{user.phone}</p>
           <a href="#/" className="btn btn-primary">
             DAHA FAZLA
           </a>
