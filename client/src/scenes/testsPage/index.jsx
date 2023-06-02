5824cdd45416f1c7f2532e62c3bfb61e4600bc9f
mehmetcandinc <dincmehmetcann@gmail.com>
2023-06-02 18:15:01 +0300

    testPage all error fixed (class->className)

--- client/src/scenes/testsPage/index.jsx
+++ client/src/scenes/testsPage/index.jsx
@@ -26,26 +26,29 @@ const TestsPage = () => {
             <div className="col-md-12">
               <div className="row">
                 <div className="col-12 col-sm-4 mt-4">
-                  <div class="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
-                    <div class="d-flex ml-4 ml-3 flex-column test-list-description">
-                      <h2 class="" id="heading0">
+                  <div className="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
+                    <div className="d-flex ml-4 ml-3 flex-column test-list-description">
+                      <h2 className="" id="heading0">
                         Anksiyete (Kaygı) Testi
                       </h2>
-                      <div class="test-list-summary">
+                      <div className="test-list-summary">
                         Yaygın Kaygı Bozukluğu 7 (GAD-7), yaygın anksiyete
                         bozukluğunun taranması ve şiddetinin ölçülmesi için
                         kullanılan bir psikoloji testidir. GAD-7, çeşitli yaygın
                         anksiyete bozukluğu belirtilerinin şiddetini ölçen yedi
                         maddeye sahiptir.
                       </div>
-                      <div class="mt-3" aria-labelledby="heading0">
-                        <a href="/tests" class="btn btn-rounded btn-primary">
+                      <div className="mt-3" aria-labelledby="heading0">
+                        <a
+                          href="/tests"
+                          className="btn btn-rounded btn-primary"
+                        >
                           Teste Başla
                         </a>
                       </div>
                     </div>
 
-                    <div class="test-list-image" aria-labelledby="heading0">
+                    <div className="test-list-image" aria-labelledby="heading0">
                       <a href="/tests">
                         <img src="img/anxiety-test.svg" alt="" loading="lazy" />
                       </a>
@@ -54,25 +57,28 @@ const TestsPage = () => {
                 </div>
 
                 <div className="col-12 col-sm-4 mt-4">
-                  <div class="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
-                    <div class="d-flex ml-4 ml-3 flex-column test-list-description">
-                      <h2 class="" id="heading0">
+                  <div className="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
+                    <div className="d-flex ml-4 ml-3 flex-column test-list-description">
+                      <h2 className="" id="heading0">
                         Depresyon Testi
                       </h2>
-                      <div class="test-list-summary">
+                      <div className="test-list-summary">
                         Hasta Sağlık Anketi 9 (PHQ-9) depresyonun taranması ve
                         şiddetinin ölçülmesi için kullanılan bir psikoloji
                         testidir. Dokuz maddeyle hızlı ve kolay bir şekilde
                         tarama yapan başarılı bir depresyon ölçeğidir.
                       </div>
-                      <div class="mt-3" aria-labelledby="heading0">
-                        <a href="/tests" class="btn btn-rounded btn-primary">
+                      <div className="mt-3" aria-labelledby="heading0">
+                        <a
+                          href="/tests"
+                          className="btn btn-rounded btn-primary"
+                        >
                           Teste Başla
                         </a>
                       </div>
                     </div>
 
-                    <div class="test-list-image" aria-labelledby="heading0">
+                    <div className="test-list-image" aria-labelledby="heading0">
                       <a href="/tests">
                         <img
                           src="img/depression-test.svg"
@@ -84,26 +90,29 @@ const TestsPage = () => {
                   </div>
                 </div>
                 <div className="col-12 col-sm-4 mt-4">
-                  <div class="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
-                    <div class="d-flex ml-4 ml-3 flex-column test-list-description">
-                      <h2 class="" id="heading0">
+                  <div className="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
+                    <div className="d-flex ml-4 ml-3 flex-column test-list-description">
+                      <h2 className="" id="heading0">
                         Öfke Testi
                       </h2>
-                      <div class="test-list-summary">
+                      <div className="test-list-summary">
                         Sürekli öfke, durumsal öfkenin genelde ne sıklıkta
                         yaşandığını anlatan bir kavramdır. Sürekli Öfke ve Öfke
                         Tarzı Ölçeği Testi ya da Öfke Testi bu durumsal öfkenin
                         yaşanma sıklığını ve öfkenin çeşitli boyutlarını ölçmek
                         amacıyla kullanılmaktadır.
                       </div>
-                      <div class="mt-3" aria-labelledby="heading0">
-                        <a href="/tests" class="btn btn-rounded btn-primary">
+                      <div className="mt-3" aria-labelledby="heading0">
+                        <a
+                          href="/tests"
+                          className="btn btn-rounded btn-primary"
+                        >
                           Teste Başla
                         </a>
                       </div>
                     </div>
 
-                    <div class="test-list-image" aria-labelledby="heading0">
+                    <div className="test-list-image" aria-labelledby="heading0">
                       <a href="/tests">
                         <img src="img/ofke-testi.svg" alt="" loading="lazy" />
                       </a>
@@ -111,26 +120,29 @@ const TestsPage = () => {
                   </div>
                 </div>
                 <div className="col-12 col-sm-4 mt-4">
-                  <div class="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
-                    <div class="d-flex ml-4 ml-3 flex-column test-list-description">
-                      <h2 class="" id="heading0">
+                  <div className="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
+                    <div className="d-flex ml-4 ml-3 flex-column test-list-description">
+                      <h2 className="" id="heading0">
                         Travma Sonrası Stres Bozukluğu Testi
                       </h2>
-                      <div class="test-list-summary">
+                      <div className="test-list-summary">
                         Travma Sonrası Stres Bozukluğu Envanteri - Sivil
                         Versiyon, Türkiye’de yaşayan bireylerde Travma Sonrası
                         Stres Bozukluğu tanı ve şiddet değerlendirmesinde rutin
                         uygulama kapsamında kullanılabilmektedir. Bu test tüm
                         DSM-IV TSSB semptomlarını sorgulayan bir ölçektir.
                       </div>
-                      <div class="mt-3" aria-labelledby="heading0">
-                        <a href="/tests" class="btn btn-rounded btn-primary">
+                      <div className="mt-3" aria-labelledby="heading0">
+                        <a
+                          href="/tests"
+                          className="btn btn-rounded btn-primary"
+                        >
                           Teste Başla
                         </a>
                       </div>
                     </div>
 
-                    <div class="test-list-image" aria-labelledby="heading0">
+                    <div className="test-list-image" aria-labelledby="heading0">
                       <a href="/tests">
                         <img
                           src="img/travma-sonrasi-stres-bozuklugu-testi.svg"
@@ -142,25 +154,28 @@ const TestsPage = () => {
                   </div>
                 </div>
                 <div className="col-12 col-sm-4 mt-4">
-                  <div class="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
-                    <div class="d-flex ml-4 ml-3 flex-column test-list-description">
-                      <h2 class="" id="heading0">
+                  <div className="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
+                    <div className="d-flex ml-4 ml-3 flex-column test-list-description">
+                      <h2 className="" id="heading0">
                         Alkol Bağımlılığı Testi
                       </h2>
-                      <div class="test-list-summary">
+                      <div className="test-list-summary">
                         AUDIT (Alkol Kullanım Bozuklukları Tanımlama Testi),
                         riskli veya tehlikeli tüketim veya herhangi bir alkol
                         kullanım bozukluğu olarak tanımlanan sağlıksız alkol
                         kullanımını taramanın basit ve etkili bir yöntemidir.
                       </div>
-                      <div class="mt-3" aria-labelledby="heading0">
-                        <a href="/tests" class="btn btn-rounded btn-primary">
+                      <div className="mt-3" aria-labelledby="heading0">
+                        <a
+                          href="/tests"
+                          className="btn btn-rounded btn-primary"
+                        >
                           Teste Başla
                         </a>
                       </div>
                     </div>
 
-                    <div class="test-list-image" aria-labelledby="heading0">
+                    <div className="test-list-image" aria-labelledby="heading0">
                       <a href="/tests">
                         <img
                           src="img/alkol-bagimliligi-testi.svg"
@@ -172,23 +187,26 @@ const TestsPage = () => {
                   </div>
                 </div>
                 <div className="col-12 col-sm-4 mt-4">
-                  <div class="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
-                    <div class="d-flex ml-4 ml-3 flex-column test-list-description">
-                      <h2 class="" id="heading0">
+                  <div className="d-flex flex-column flex-sm-row align-items-center border border-secondary rounded p-3 test-list-box">
+                    <div className="d-flex ml-4 ml-3 flex-column test-list-description">
+                      <h2 className="" id="heading0">
                         Stres Testi
                       </h2>
-                      <div class="test-list-summary">
+                      <div className="test-list-summary">
                         DASS21, son 1 haftadır mevcut olan stres belirtilerini
                         ölçen 7 maddeyi içeren bir araçtır.
                       </div>
-                      <div class="mt-3" aria-labelledby="heading0">
-                        <a href="/tests" class="btn btn-rounded btn-primary">
+                      <div className="mt-3" aria-labelledby="heading0">
+                        <a
+                          href="/tests"
+                          className="btn btn-rounded btn-primary"
+                        >
                           Teste Başla
                         </a>
                       </div>
                     </div>
 
-                    <div class="test-list-image" aria-labelledby="heading0">
+                    <div className="test-list-image" aria-labelledby="heading0">
                       <a href="/tests">
                         <img src="img/stress-test.svg" alt="" loading="lazy" />
                       </a>
