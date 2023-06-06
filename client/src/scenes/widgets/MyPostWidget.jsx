d5929923453d163ac7f05b03d4771667a2f9db2d
Atakan Erkan <atakan.erkan@ogr.dpu.edu.tr>
2023-06-06 22:44:50 +0300

    added isLoading

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -25,6 +25,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import { useNavigate } from "react-router-dom";
+import Loader from "scenes/loaderPage/Loader";
 
 const MyPostWidget = ({ picturePath, userId }) => {
   const dispatch = useDispatch();
@@ -38,6 +39,7 @@ const MyPostWidget = ({ picturePath, userId }) => {
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
   const userID = userId ? `${user.firstName}.${user.lastName}` : userId;
+  const [isLoading, setIsLoading] = useState(false);
 
   const handlePost = async () => {
     const formData = new FormData();
@@ -54,113 +56,129 @@ const MyPostWidget = ({ picturePath, userId }) => {
       body: formData,
     });
     const posts = await response.json();
-    navigate(`/posts`);
     dispatch(setPosts({ posts }));
     setImage(null);
     setPost("");
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      window.location.reload();
+    }, 1000);
   };
 
   return (
-    <WidgetWrapper>
-      <FlexBetween gap="1.5rem">
-        <UserImage image={picturePath} size="80px" />
-        <InputBase
-          placeholder="Aklınızdan ne geçiyor..."
-          onChange={(e) => setPost(e.target.value)}
-          value={post}
-          sx={{
-            width: "100%",
-            backgroundColor: palette.neutral.light,
-            borderRadius: "2rem",
-            padding: "1rem 2rem",
-          }}
-        />
-      </FlexBetween>
-      {isImage && (
-        <Box border={`1px solid #175c4c`} borderRadius="5px" mt="1rem" p="1rem">
-          <Dropzone
-            acceptedFiles=".jpg,.jpeg,.png"
-            multiple={false}
-            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
-          >
-            {({ getRootProps, getInputProps }) => (
-              <FlexBetween>
-                <Box
-                  {...getRootProps()}
-                  border={`2px dashed ${palette.primary.main}`}
-                  p="1rem"
-                  width="100%"
-                  sx={{ "&:hover": { cursor: "pointer" } }}
-                >
-                  <input {...getInputProps()} />
-                  {!image ? (
-                    <p>Resminizi Ekleyin</p>
-                  ) : (
-                    <FlexBetween>
-                      <Typography>{image.name}</Typography>
-                      <EditOutlined />
-                    </FlexBetween>
-                  )}
-                </Box>
-                {image && (
-                  <IconButton
-                    onClick={() => setImage(null)}
-                    sx={{ width: "15%" }}
-                  >
-                    <DeleteOutlined />
-                  </IconButton>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <WidgetWrapper>
+          <FlexBetween gap="1.5rem">
+            <UserImage image={picturePath} />
+            <InputBase
+              placeholder="Aklınızdan ne geçiyor..."
+              onChange={(e) => setPost(e.target.value)}
+              value={post}
+              sx={{
+                width: "100%",
+                backgroundColor: palette.neutral.light,
+                borderRadius: "2rem",
+                padding: "1rem 2rem",
+              }}
+            />
+          </FlexBetween>
+          {isImage && (
+            <Box
+              border={`1px solid #175c4c`}
+              borderRadius="5px"
+              mt="1rem"
+              p="1rem"
+            >
+              <Dropzone
+                acceptedFiles=".jpg,.jpeg,.png"
+                multiple={false}
+                onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+              >
+                {({ getRootProps, getInputProps }) => (
+                  <FlexBetween>
+                    <Box
+                      {...getRootProps()}
+                      border={`2px dashed ${palette.primary.main}`}
+                      p="1rem"
+                      width="100%"
+                      sx={{ "&:hover": { cursor: "pointer" } }}
+                    >
+                      <input {...getInputProps()} />
+                      {!image ? (
+                        <p>Resminizi Ekleyin</p>
+                      ) : (
+                        <FlexBetween>
+                          <Typography>{image.name}</Typography>
+                          <EditOutlined />
+                        </FlexBetween>
+                      )}
+                    </Box>
+                    {image && (
+                      <IconButton
+                        onClick={() => setImage(null)}
+                        sx={{ width: "15%" }}
+                      >
+                        <DeleteOutlined />
+                      </IconButton>
+                    )}
+                  </FlexBetween>
                 )}
-              </FlexBetween>
-            )}
-          </Dropzone>
-        </Box>
-      )}
+              </Dropzone>
+            </Box>
+          )}
 
-      <Divider sx={{ margin: "1.25rem 0" }} />
+          <Divider sx={{ margin: "1.25rem 0" }} />
 
-      <FlexBetween>
-        <FlexBetween gap="0.25rem" onClick={() => setIsImage(!isImage)}>
-          <ImageOutlined sx={{ cursor: "pointer", color: "#175c4c" }} />
-          <Typography
-            color={"#175c4c"}
-            sx={{ "&:hover": { cursor: "pointer", color: "#175c4c" } }}
-          >
-            Medya
-          </Typography>
-        </FlexBetween>
-
-        {isNonMobileScreens ? (
-          <>
-            <FlexBetween gap="0.25rem">
-              <MicOutlined sx={{ color: "#175c4c", cursor: "pointer" }} />
-              <Typography sx={{ color: "#175c4c", cursor: "pointer" }}>
-                Ses
+          <FlexBetween>
+            <FlexBetween gap="0.25rem" onClick={() => setIsImage(!isImage)}>
+              <ImageOutlined sx={{ cursor: "pointer", color: "#175c4c" }} />
+              <Typography
+                color={"#175c4c"}
+                sx={{ "&:hover": { cursor: "pointer", color: "#175c4c" } }}
+              >
+                Medya
               </Typography>
             </FlexBetween>
-          </>
-        ) : (
-          <FlexBetween gap="0.25rem">
-            <MoreHorizOutlined sx={{ color: "#175c4c" }} />
-          </FlexBetween>
-        )}
 
-        <Button
-          disabled={!post}
-          onClick={handlePost}
-          className="btn"
-          style={{
-            color: "#fff",
-            backgroundColor: "#175c4c",
-            opacity: "1",
-            letterSpacing: "1px",
-            fontFamily: "Sofia Pro",
-            cursor: "pointer",
-          }}
-        >
-          Paylaş
-        </Button>
-      </FlexBetween>
-    </WidgetWrapper>
+            {isNonMobileScreens ? (
+              <>
+                <FlexBetween gap="0.25rem">
+                  <MicOutlined sx={{ color: "#175c4c", cursor: "pointer" }} />
+                  <Typography sx={{ color: "#175c4c", cursor: "pointer" }}>
+                    Ses
+                  </Typography>
+                </FlexBetween>
+              </>
+            ) : (
+              <FlexBetween gap="0.25rem">
+                <MoreHorizOutlined sx={{ color: "#175c4c" }} />
+              </FlexBetween>
+            )}
+
+            <Button
+              disabled={!post}
+              onClick={handlePost}
+              className="btn"
+              style={{
+                color: "#fff",
+                backgroundColor: "#175c4c",
+                opacity: "1",
+                letterSpacing: "1px",
+                fontFamily: "Sofia Pro",
+                cursor: "pointer",
+              }}
+            >
+              Paylaş
+            </Button>
+          </FlexBetween>
+        </WidgetWrapper>
+      )}
+    </>
   );
 };
 
